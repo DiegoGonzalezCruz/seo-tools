d46dfc2efc13d696b86d7deb9ba2bd87053e7c17
Diego González Cruz <diego@thinkey.cl>
2025-01-05 20:54:50 -0500

    landing page minimal

--- src/components/Pages/LandingPage/Landing.jsx
+++ src/components/Pages/LandingPage/Landing.jsx
@@ -13,8 +13,8 @@ export default function LandingPage() {
         <main className="w-full">
           <Hero />
           <Features />
-          <Contact />
-          <HelpCenter />
+          {/* <Contact /> */}
+          {/* <HelpCenter /> */}
         </main>
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
           <p className="text-xs text-gray-500">
